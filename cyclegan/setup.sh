f26aeb64a1a5bf734a2397e64ecc7f0f617dda13
Oshan Mudannayake <oshan.ivantha@gmail.com>
2021-09-05 04:23:49 +0530

    Add cyclegan

---
+++ cyclegan/setup.sh
@@ -0,0 +1,3 @@
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
+conda install -y -c conda-forge dominate
+pip install visdom
---
+++ cyclegan/train.sh
@@ -0,0 +1,3 @@
+python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan
+
+#python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan --continue_train --epoch_count 5801
