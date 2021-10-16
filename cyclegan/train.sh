ebbb0e555ea97fab7328278b3009903c243f79ff
Oshan Ivantha <oshan.ivantha@gmail.com>
2021-10-16 21:32:33 +0530

    Update cyclegan

--- cyclegan/train.sh
+++ cyclegan/train.sh
@@ -1,3 +1,5 @@
-python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan
+#python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan
 
 #python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan --continue_train --epoch_count 5801
+
+python train.py --dataroot ./datasets/leaf --name leaf_cyclegan --model cycle_gan --continue_train --epoch_count 8000
